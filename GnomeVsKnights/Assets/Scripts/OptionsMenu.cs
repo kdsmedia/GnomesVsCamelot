8c258395a8dc69af2154137806353674694ae81d
phoenixgoldz <thicks@student.neumont.edu>
2025-02-19 08:50:26 -0700

    sound and button fixes
    - updated the sound files to click all buttons
    - fixed buttons settings to navigate correctly
    - NEXT WAVE button on winner panel needs to be fixed to start next round (so for now its disabled)
    - fixed mainemenu options panel size to fit correctly on screen

--- GnomeVsKnights/Assets/Scripts/OptionsMenu.cs
+++ GnomeVsKnights/Assets/Scripts/OptionsMenu.cs
@@ -8,7 +8,6 @@ public class OptionsMenu : MonoBehaviour
 {
     public GameObject optionsPanel;
    
-    public Toggle subtitlesToggle;
     public Button applyButton;
     public Button restoreDefaultsButton;
     public Button closeButton;
@@ -18,8 +17,11 @@ public class OptionsMenu : MonoBehaviour
     public Image savingImage;
     public RectTransform savingImageTransform;
 
-    private float tempMusicVolume = 0.5f;
-    private float tempSoundVolume = 0.5f;
+    [Header("Sliders")]
+    public Slider musicSlider;
+    public Slider soundSlider;
+
+    private const float DEFAULT_VOLUME = 0.5f; // 50% Default Volume
 
     private void Start()
     {
@@ -27,28 +29,42 @@ public class OptionsMenu : MonoBehaviour
         saveMessage.gameObject.SetActive(false);
         savingImage.gameObject.SetActive(false);
         
-        restoreDefaultsButton.onClick.AddListener(() => { PlayButtonSound(); });
+        restoreDefaultsButton.onClick.AddListener(RestoreDefaults);
         closeButton.onClick.AddListener(() => { PlayButtonSound(); CloseOptions(); });
+
+        LoadSettings();
     }
 
-    public void OpenOptions()
+    private void LoadSettings()
     {
-        optionsPanel.SetActive(true);
+        float musicVolume = PlayerPrefs.GetFloat("MusicVolume", DEFAULT_VOLUME);
+        float soundVolume = PlayerPrefs.GetFloat("SoundVolume", DEFAULT_VOLUME);
+
+        musicSlider.value = musicVolume;
+        soundSlider.value = soundVolume;
+    }
+
+    public void RestoreDefaults()
+    {
         PlayButtonSound();
-    }
 
-    public void CloseOptions()
-    {
-        optionsPanel.SetActive(false);
+        // Reset sliders to 50%
+        musicSlider.value = DEFAULT_VOLUME;
+        soundSlider.value = DEFAULT_VOLUME;
+
+        // Apply new settings
+        ApplySettings();
     }
 
     public void ApplySettings()
     {
-        PlayerPrefs.SetFloat("MusicVolume", tempMusicVolume);
-        PlayerPrefs.SetFloat("SoundVolume", tempSoundVolume);
+        PlayerPrefs.SetFloat("MusicVolume", musicSlider.value);
+        PlayerPrefs.SetFloat("SoundVolume", soundSlider.value);
         PlayerPrefs.Save();
 
-        Debug.LogFormat("Settings Applied: Music Volume = " + (tempMusicVolume * 100) + "%", "SoundVolume = " + (tempSoundVolume * 100) + "%");
+        Debug.LogFormat("Settings Applied: Music Volume = " + (musicSlider.value * 100) + "%", 
+                        "Sound Volume = " + (soundSlider.value * 100) + "%");
+
         StartCoroutine(ShowSaveMessage());
     }
 
@@ -72,10 +88,15 @@ public class OptionsMenu : MonoBehaviour
 
     private void PlayButtonSound()
     {
-        if (buttonClickAudioSource != null && buttonClickSound != null)
+        if (AudioManager.Instance != null)
         {
-            buttonClickAudioSource.PlayOneShot(buttonClickSound);
+            AudioManager.Instance.PlaySFX(AudioManager.Instance.buttonClickSound);
         }
     }
-    
+
+    // 🔹 FIXED: This method now exists
+    public void CloseOptions()
+    {
+        optionsPanel.SetActive(false);
+    }
 }
